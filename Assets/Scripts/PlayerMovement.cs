f3cdb3d5f94493eb1f43ec732e2afeb44ec6c9df
Iniwizzy <badawilovina@gmail.com>
2025-06-14 17:23:46 +0700

    First commit with initial Unity project structure

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private bool grounded;
+    private bool jump;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        //Get reference for rigidbidy from object
+        body = GetComponent<Rigidbody2D>();
+
+        //Get reference for animator from object
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+
+        //Flip player when moving left-right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+        anim.SetBool("jump", jump);
+    }
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        grounded = false;
+        jump = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            jump = false;
+        }
+    }
+
+    
+}
